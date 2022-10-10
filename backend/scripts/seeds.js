4b9469c3a7d8cbac1d01625341bd476d2227aa4f
Ohad Basan <ohad.basan@gmail.com>
2022-10-10 15:12:29 +0300

    seed script

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,40 @@
-//TODO: seeds script should come here, so we'll be able to put some data in our local env
+require("dotenv").config();
+require("../models/User");
+require("../models/Item");
+require("../models/Comment");
+
+
+var mongoose = require("mongoose");
+console.log(process.env)
+mongoose.connect(process.env.MONGODB_URI);
+var User = mongoose.model("User");
+var Comment = mongoose.model("Comment");
+var Item = mongoose.model("Item");
+
+
+
+for (let i = 0; i < 100; i++) {
+  var user = new User();
+  let r = (Math.random() + 1).toString(36).substring(7);
+  user.username = r
+  user.email = `"a${i}@a.com"`;
+  user.setPassword(i.toString());
+  user.save().catch(console.error('failed'));
+}
+
+for (let i = 0; i < 100; i++) {
+  var comment = new Comment();
+  let r = (Math.random() + 1).toString(36).substring(7);
+  comment.body = r
+  comment.save().catch(console.error('failed'));
+}
+
+for (let i = 0; i < 100; i++) {
+  var item = new Item();
+  let r = (Math.random() + 1).toString(36).substring(7);
+  item.title = r
+  item.description = `"a${i}@a.com"`;
+  item.save().catch(console.error('failed'));
+}
+
+
