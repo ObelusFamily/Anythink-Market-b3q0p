951743c82023e3e7344494c25a868dea7b1a0d2e
Ohad Basan <ohad.basan@gmail.com>
2022-10-10 15:38:05 +0300

    fix async flow

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -5,36 +5,48 @@ require("../models/Comment");
 
 
 var mongoose = require("mongoose");
-console.log(process.env)
+
 mongoose.connect(process.env.MONGODB_URI);
 var User = mongoose.model("User");
 var Comment = mongoose.model("Comment");
 var Item = mongoose.model("Item");
 
+function run() {
 
-
-for (let i = 0; i < 100; i++) {
-  var user = new User();
-  let r = (Math.random() + 1).toString(36).substring(7);
-  user.username = r
-  user.email = `"a${i}@a.com"`;
-  user.setPassword(i.toString());
-  user.save().catch(console.error('failed'));
+    promises = []
+    for (let i = 0; i < 100; i++) {
+      var user = new User();
+      let r = (Math.random() + 1).toString(36).substring(7);
+      user.username = r
+      user.email = `"a${r}@a.com"`;
+      user.setPassword(i.toString());
+      promises.push(user.save())
+    }
+    
+    for (let i = 0; i < 100; i++) {
+      var comment = new Comment();
+      let r = (Math.random() + 1).toString(36).substring(7);
+      comment.body = r
+      promises.push(comment.save())
+    }
+    
+    for (let i = 0; i < 100; i++) {
+      var item = new Item();
+      let r = (Math.random() + 1).toString(36).substring(7);
+      item.title = r
+      item.description = `"a${i}@a.com"`;
+      promises.push(item.save())
+    }
+    return Promise.all(promises)
 }
 
-for (let i = 0; i < 100; i++) {
-  var comment = new Comment();
-  let r = (Math.random() + 1).toString(36).substring(7);
-  comment.body = r
-  comment.save().catch(console.error('failed'));
-}
-
-for (let i = 0; i < 100; i++) {
-  var item = new Item();
-  let r = (Math.random() + 1).toString(36).substring(7);
-  item.title = r
-  item.description = `"a${i}@a.com"`;
-  item.save().catch(console.error('failed'));
-}
 
 
+
+
+run().then(function() {
+  console.log('finished')
+  mongoose.connection.close()
+}).catch(function(e) {
+  console.log(e)
+})
